f9cc6aa1d93cb8353a7f061780dea18aefb8b187
ZYYX VietNam <zyyxvietnam@ZYYXs-MacBook-Pro-2.local>
2024-04-15 17:37:03 +0700

    Add Api

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 class ColorConstant{
   static const Color themeColor = Colors.black;
 }
+class DeezerOptions {
+  static const String baseUrl = "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem";
+  static const headers = {
+  'X-RapidAPI-Key': '493c0fb7d5mshe4c228aab01083cp1edc00jsna60d541227b1',
+  'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+  };
+}
--- lib/controller/home_page_controller.dart
+++ lib/controller/home_page_controller.dart
@@ -1,9 +1,36 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:music_app/model/audio.dart';
+import 'package:music_app/view/page/home_scene.dart';
+
+import '../repositories/list_audio_repository.dart';
 
 class HomePageController extends GetxController {
   var currentIndex = 0.obs;
+  var isLoading = true.obs;
+  var _player;
+
+  get player => _player;
+
+  set player(value) {
+    _player = value;
+  }
+  var listAudio = ListAudio();
+  @override
+  Future<void> onInit() async {
+    player = AudioPlayer();
+    listAudio = (await AudioRepository().fetchData())!;
+    isLoading.value = false;
+    super.onInit();
+  }
+  Widget currentPage = const HomeScene();
+  var listPage = [const HomeScene(), const Scaffold()];
   void changeCurrent(int index){
     currentIndex.value = index;
+    currentPage = listPage[index];
   }
-
+  Future<void> playAudio(int index) async {
+    await player.play(UrlSource(listAudio.data![index].preview!));
+  }
 }
